ee2a94dd9cfbc318b38a996e41a6da8d1fb599d0
daniel jaimes <james.rock.alt@gmail.com>
2020-03-04 10:59:38 -0400

    validaciones registerform

--- src/app/registrate/registrate.module.ts
+++ src/app/registrate/registrate.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { RegistratePageRoutingModule } from './registrate-routing.module';
 
 import { RegistratePage } from './registrate.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistratePageRoutingModule
+    RegistratePageRoutingModule,
+    ReactiveFormsModule,// importamos el formsmodule
+    ComponentsModule,//componentes
   ],
   declarations: [RegistratePage]
 })
--- src/app/registrate/registrate.page.ts
+++ src/app/registrate/registrate.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MetodosService } from '../servicios/metodos.service';
 import { Router } from '@angular/router';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-registrate',
@@ -8,21 +9,55 @@ import { Router } from '@angular/router';
   styleUrls: ['./registrate.page.scss'],
 })
 export class RegistratePage implements OnInit {
-  email:string
-  password:string
-  nombre:string
-  telefono:string
-  constructor( private metodos:MetodosService,private router:Router) { }
+  email: string
+  password: string
+  nombre: string
+  telefono: string
+  registerForm:FormGroup
+  constructor(private metodos: MetodosService, private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.registerForm = this.formBuilder.group({
+      email: ["", [
+        Validators.required,
+        //Validators.pattern('[A-Za-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})'),
+        Validators.email
+      ]
+      ],
+      password: ["", [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20),
+      ]
+      ],
+      confirm_password: [""],
+      nombre: ["", Validators.required],
+      telefono: ["", [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(15)
+      ]],
+    });
+    this.registerForm.get('confirm_password').setValidators([
+      this.metodos.CustomValidators.equals(this.registerForm.get('password')),
+      Validators.required
+    ]);
+  }
 
   ngOnInit() {
   }
 
-  registrate(){
-    
-    this.metodos.registro(this.email,this.password,this.nombre,this.telefono).then(res =>{
+  registrate() {
+    this.registerForm.value
+		delete this.registerForm.value.confirm_password
+    //this.metodos.registro(this.email, this.password, this.nombre, this.telefono).then(res => {
+    //  this.router.navigate(['/tabs/tab1'])
+    //}).catch(err => console.log(err));
+    this.metodos.registro2(this.registerForm.value).then(() => {
       this.router.navigate(['/tabs/tab1'])
+      console.log("aceptado");
       
-    }).catch(err=> console.log(err));
+    }).catch(err => console.log(err));
   }
 
 }
